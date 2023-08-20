3628e83d32693d45676e85059be91ee4615ac566
isok <isok7071@gmail.com>
2023-08-20 13:55:08 +0300

    Создает модель User

---
+++ app/backend/models/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use \yii\db\ActiveRecord;
+
+/**
+ * Summary of User
+ */
+class User extends ActiveRecord implements \yii\web\IdentityInterface
+{
+    /**
+     * 
+     * @return string
+     */
+    public static function tableName(): string
+    {
+        return 'user';
+    }
+
+    /**
+     * Валидирует поля модели
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            [['first_name', 'last_name', 'email', 'auth_key'], 'required', 'message' => 'Поле обязательно для заполения'],
+            ['email', 'email', 'message' => 'Email введен неверно'],
+            ['access_token', 'unique', 'message' => 'Уже есть такое значение'],
+            ['email', 'unique', 'message' => 'Email уже зарегистрирован'],
+
+        ];
+    }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    public function setAuthKey()
+    {
+        $this->auth_key = Yii::$app->getSecurity()->generateRandomString();
+    }
+}
