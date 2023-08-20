8dbd5d38623891896b3185e2c37436a3e48ee101
isok <isok7071@gmail.com>
2023-08-20 13:57:04 +0300

    Инициализирует проект на Vue (vite+vuetify+pinia)

---
+++ app/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <registration-form></registration-form>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import RegistrationForm from '@/components/RegistrationForm.vue';
+</script>
